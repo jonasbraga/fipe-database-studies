be0524d51121447491062aa8f8dc227e1c1656ca
Jonas <jonasbraga2001@gmail.com>
2023-10-31 15:41:03 -0300

    feat: :sparkles: adding example data from fipe API

---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+import makes from '../examples/makes.json'
+import models from '../examples/models.json'
+import years from '../examples/years.json'
+import details from '../examples/details.json'
+
+export type FipeMakes = typeof makes
+export type FipeModels = typeof models
+export type FipeYears = typeof years
+export type FipeDetails = typeof details
